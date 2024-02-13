2dde1ac7fa7d056fd2e209314c1a43aae89a3ceb
RoboRobert <nathan.estep@gmail.com>
2024-02-12 20:33:38 -0600

    defines what I believe to be a fairly reasonable base project structure.

---
+++ src/hello_wordle/Hello_Wordle.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hello_wordle;
+
+/**
+ *
+ * @author nathan
+ */
+public class Hello_Wordle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello Wordle!");
+    }
+    
+}
