7929b0211a94dd531789aec694f5146f89b1b1cf
tessmr <89546848+tessmr@users.noreply.github.com>
2024-04-21 19:39:24 -0500

    Update GuessHandler.java
    Updated file to handle paradigm as an int instead of a string

--- src/model/GuessHandler.java
+++ src/model/GuessHandler.java
@@ -35,7 +35,7 @@ public class GuessHandler {
      */
     public static Guess getGuess(String name) {
 //        TODO Add implementation
-        Guess returnGuess = new Guess("Java", "STATIC", "OO", "HIGH", 1995);
+        Guess returnGuess = new Guess("Java", "STATIC", 3, "HIGH", 1995);
 
         return returnGuess;
     }
@@ -66,7 +66,7 @@ public class GuessHandler {
      * 
      * @return Returns a Color used to change the color of the paradigm guess slot
      */
-    public static Color matchParadigm(String paradigm1, String paradigm2) {
+    public static Color matchParadigm(int paradigm1, int paradigm2) {
         if(paradigm1 == paradigm2)
             return MY_GREEN;
         
